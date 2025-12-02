22281f17c838a2693fbb8da9689851e61d88d852
Admin <Admin@DESKTOP-MAHVHFA>
2025-12-02 20:24:00 +0300

    Добавьте файлы проекта.

---
+++ FolderModels/Model1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace РКИС_ЛР1.FolderModels
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=ModelDB")
+        {
+        }
+
+        public virtual DbSet<Motorbike> Motorbike { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
