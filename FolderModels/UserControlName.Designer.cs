57d637ad8346d0e071b25f1566df52b4c1802732
Admin <Admin@DESKTOP-MAHVHFA>
2025-12-02 20:34:03 +0300

    Создание формы

---
+++ FolderModels/UserControlName.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace РКИС_ЛР1.FolderModels
+{
+    partial class UserControlName
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelMileageData = new System.Windows.Forms.Label();
+            this.labelHorsepowerData = new System.Windows.Forms.Label();
+            this.labelPriceData = new System.Windows.Forms.Label();
+            this.labelBrandData = new System.Windows.Forms.Label();
+            this.labelModelData = new System.Windows.Forms.Label();
+            this.labelIDData = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(44, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Модель";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Цена";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Марка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 472);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Пробег";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Л/С";
+            // 
+            // labelMileageData
+            // 
+            this.labelMileageData.AutoSize = true;
+            this.labelMileageData.Location = new System.Drawing.Point(194, 472);
+            this.labelMileageData.Name = "labelMileageData";
+            this.labelMileageData.Size = new System.Drawing.Size(179, 25);
+            this.labelMileageData.TabIndex = 18;
+            this.labelMileageData.Text = "labelMileageData";
+            // 
+            // labelHorsepowerData
+            // 
+            this.labelHorsepowerData.AutoSize = true;
+            this.labelHorsepowerData.Location = new System.Drawing.Point(194, 424);
+            this.labelHorsepowerData.Name = "labelHorsepowerData";
+            this.labelHorsepowerData.Size = new System.Drawing.Size(218, 25);
+            this.labelHorsepowerData.TabIndex = 17;
+            this.labelHorsepowerData.Text = "labelHorsepowerData";
+            // 
+            // labelPriceData
+            // 
+            this.labelPriceData.AutoSize = true;
+            this.labelPriceData.Location = new System.Drawing.Point(194, 374);
+            this.labelPriceData.Name = "labelPriceData";
+            this.labelPriceData.Size = new System.Drawing.Size(152, 25);
+            this.labelPriceData.TabIndex = 16;
+            this.labelPriceData.Text = "labelPriceData";
+            // 
+            // labelBrandData
+            // 
+            this.labelBrandData.AutoSize = true;
+            this.labelBrandData.Location = new System.Drawing.Point(194, 326);
+            this.labelBrandData.Name = "labelBrandData";
+            this.labelBrandData.Size = new System.Drawing.Size(160, 25);
+            this.labelBrandData.TabIndex = 15;
+            this.labelBrandData.Text = "labelBrandData";
+            // 
+            // labelModelData
+            // 
+            this.labelModelData.AutoSize = true;
+            this.labelModelData.Location = new System.Drawing.Point(194, 275);
+            this.labelModelData.Name = "labelModelData";
+            this.labelModelData.Size = new System.Drawing.Size(162, 25);
+            this.labelModelData.TabIndex = 14;
+            this.labelModelData.Text = "labelModelData";
+            // 
+            // labelIDData
+            // 
+            this.labelIDData.AutoSize = true;
+            this.labelIDData.Location = new System.Drawing.Point(194, 227);
+            this.labelIDData.Name = "labelIDData";
+            this.labelIDData.Size = new System.Drawing.Size(123, 25);
+            this.labelIDData.TabIndex = 13;
+            this.labelIDData.Text = "labelIDData";
+            // 
+            // UserControlName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelMileageData);
+            this.Controls.Add(this.labelHorsepowerData);
+            this.Controls.Add(this.labelPriceData);
+            this.Controls.Add(this.labelBrandData);
+            this.Controls.Add(this.labelModelData);
+            this.Controls.Add(this.labelIDData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControlName";
+            this.Size = new System.Drawing.Size(362, 536);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelMileageData;
+        private System.Windows.Forms.Label labelHorsepowerData;
+        private System.Windows.Forms.Label labelPriceData;
+        private System.Windows.Forms.Label labelBrandData;
+        private System.Windows.Forms.Label labelModelData;
+        private System.Windows.Forms.Label labelIDData;
+    }
+}
---
+++ FolderModels/UserControlName.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace РКИС_ЛР1.FolderModels
+{
+    public partial class UserControlName : UserControl
+    {
+        public UserControlName()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
